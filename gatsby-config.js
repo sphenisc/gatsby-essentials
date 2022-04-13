77153efc3b6cf45b0126211636340f3c16422d66
yokoyama <spenic635@gmail.com>
2022-04-13 10:14:27 +0900

    Chapter2終わり

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "mysite20211120",
-  },
-  plugins: [],
+	siteMetadata: {
+		siteUrl: "https://www.yourdomain.tld",
+		title: "mysite20211120",
+	},
+	plugins: [
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images/`,
+			},
+		},
+	],
 };
